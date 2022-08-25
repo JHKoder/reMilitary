b744647573edbdb6077354b48d73bbaf0518ac8f
oiNeh <kgh22522252@gmail.com>
2022-08-25 19:41:08 +0900

    :recycle: Main 코드 수정 및 인코딩 변경

---
+++ pingTest/src/Main.java
@@ -0,0 +1,31 @@
+package src;
+
+import static src.ReadText.getTextIpList;
+
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String postIp = "127.0.0.1";
+
+        List<String> ipList = getTextIpList();
+        int ipLen = ipList.size();
+
+        Pingthread[] pings = new Pingthread[ipLen];
+
+        for (int i = 0; i < ipLen; i++) {
+            pings[i] = new Pingthread(ipList.get(i), i, postIp);
+            pings[i].start();
+        }
+
+        Workping workping = new Workping(pings, ipLen);
+
+        for (int i = 0; i < ipLen; i++) {
+            pings[i].setWorkping(workping);
+        }
+
+    }
+
+}
